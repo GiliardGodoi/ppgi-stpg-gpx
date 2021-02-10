ae20bee5daf49cf2a6e0bd027a8c00680a61a296
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2021-02-10 12:05:54 -0300

    add test_resultant_cost and refact test_simpliest

--- tests/edgeset/test_mutate.py
+++ tests/edgeset/test_mutate.py
@@ -2,6 +2,7 @@ from os import path
 import unittest
 
 from ga4stpg.edgeset import EdgeSet
+from ga4stpg.edgeset.evaluation import EvaluateEdgeSet
 from ga4stpg.edgeset.generate import gen_random_prim
 from ga4stpg.edgeset.mutate import MutateReconectingComponents
 from ga4stpg.graph import UGraph
@@ -11,18 +12,22 @@ from ga4stpg.graph.util import is_steiner_tree
 
 class TestMutateReconectingComponents(unittest.TestCase):
 
-    def test_simpliest(self):
+    def setUp(self) -> None:
         filename = path.join('datasets', 'ORLibrary', 'steinb18.txt')
-        stpg  = ReaderORLibrary().parser(filename)
+        self.stpg  = ReaderORLibrary().parser(filename)
+
+    def test_simpliest(self):
+        stpg  = self.stpg
 
         mutate = MutateReconectingComponents(stpg)
 
         after = gen_random_prim(stpg)
+
         self.assertIsInstance(after, EdgeSet)
 
         before = mutate(after)
 
-        self.assertNotEqual(after, before)
+        self.assertIsInstance(before, EdgeSet)
 
         tree = UGraph()
 
@@ -36,6 +41,20 @@ class TestMutateReconectingComponents(unittest.TestCase):
         self.assertTrue(response['all_edges_are_reliable'])
         self.assertTrue(response['graph_is_connected'])
         # It is not possible to grant that 'all_leaves_are_terminals'
+
+    def test_resultant_cost(self):
+        stpg  = self.stpg
+
+        mutate = MutateReconectingComponents(stpg)
+        evaluate = EvaluateEdgeSet(stpg)
+
+        after = gen_random_prim(stpg)
+        before = mutate(after)
+
+        after_cost, _  = evaluate(after)
+        before_cost, _ = evaluate(before)
+
+        self.assertGreaterEqual(after_cost, before_cost)
 
 if __name__ == "__main__":
     unittest.main()
