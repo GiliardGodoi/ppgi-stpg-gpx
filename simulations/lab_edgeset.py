39bbb4489c38dd9bfbf5625c5531bc94d88c038d
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2021-02-13 14:47:09 -0300

    add simulation_kruskal_crossover and refact simulation
    simulation_random_walk

--- simulations/lab_edgeset.py
+++ simulations/lab_edgeset.py
@@ -1,6 +1,6 @@
 import os
 
-from ga4stpg.edgeset.crossover import CrossoverRandomWalkRST
+from ga4stpg.edgeset.crossover import CrossoverRandomWalkRST, CrossoverKruskalRST
 from ga4stpg.edgeset.evaluation import EvaluateEdgeSet
 from ga4stpg.edgeset.generate import gen_random_walk
 from ga4stpg.edgeset.mutate import MutationReplaceByRandomEdge
@@ -15,7 +15,7 @@ from ga4stpg.tracker import DataTracker
 from ga4stpg.util import STEIN_B, display, update_best, update_generation
 
 
-def simulation(params):
+def simulation_random_walk(params):
 
     STPG = read_problem("datasets", "ORLibrary", params["dataset"])
 
@@ -24,6 +24,45 @@ def simulation(params):
     mutate = MutationReplaceByRandomEdge(STPG)
 
     output_data_dir = os.path.join("data","test","edgeset", STPG.name)
+    tracker = DataTracker(params['runtrial'],target=output_data_dir)
+
+    population = (GPopulation(
+        chromosomes=[gen_random_walk(STPG) for _ in range(params["population_size"])],
+        eval_function=evaluation,
+        maximize=True)
+    .evaluate()
+    .normalize(norm_function=normalize)
+    .callback(update_best))
+
+    evol = (Evolution()
+                .evaluate()
+                .normalize(norm_function=normalize)
+                .callback(update_best)
+                .callback(tracker.log_evaluation)
+                .select(selection_func=roullete)
+                .crossover(combiner=crossover)
+                .mutate(mutate_function=mutate, probability=params['tx_mutation'])
+                .callback(update_generation)
+                .callback(display, every=100))
+
+    with Stagnation(interval=params["stagnation_interval"]), \
+        BestKnownReached(global_optimum=params['global_optimum']):
+        result = population.evolve(evol, n=params["n_iterations"])
+
+    tracker.log_simulation(params, STPG, result)
+    tracker.report()
+
+    print(result.stoppedby)
+
+def simulation_kruskal_crossover(params):
+
+    STPG = read_problem("datasets", "ORLibrary", params["dataset"])
+
+    crossover = CrossoverKruskalRST(STPG)
+    evaluation = EvaluateEdgeSet(STPG)
+    mutate = MutationReplaceByRandomEdge(STPG)
+
+    output_data_dir = os.path.join("data","test","edgeset", "kruskal_crossover", STPG.name)
     tracker = DataTracker(params['runtrial'],target=output_data_dir)
 
     population = (GPopulation(
@@ -74,4 +113,4 @@ if __name__ == "__main__":
         PARAMS['global_optimum'] = value
         for i in range(30):
             PARAMS['runtrial'] = i + 1
-            simulation(params=PARAMS)
+            simulation_kruskal_crossover(params=PARAMS)
