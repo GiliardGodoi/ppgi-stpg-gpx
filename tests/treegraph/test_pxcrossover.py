f71f2a9f77f656eb3bf083418fcd033bb15a76b5
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2021-02-14 10:54:27 -0300

    implement basic test for pxcrossover

---
+++ tests/treegraph/test_pxcrossover.py
@@ -0,0 +1,62 @@
+from ga4stpg.graph.reader import ReaderORLibrary
+import unittest
+from os import path
+from random import sample
+
+from ga4stpg.tree.pxcrossover import PXtree
+from ga4stpg.graph import ReaderORLibrary, UGraph
+from ga4stpg.graph.steiner import shortest_path_with_origin
+from ga4stpg.graph.util import is_steiner_tree, gg_total_weight
+
+class TestPXCrossover(unittest.TestCase):
+
+    def test_basic(self):
+        filename = path.join("datasets", "ORLibrary", "steinc5.txt")
+        stpg = ReaderORLibrary().parser(filename)
+
+        vertices = list(stpg.graph.vertices)
+        v, u = sample(vertices, 2)
+
+        crossover = PXtree(stpg)
+
+        red, red_cost   = shortest_path_with_origin(stpg.graph, v, stpg.terminals)
+        blue, blue_cost = shortest_path_with_origin(stpg.graph, u, stpg.terminals)
+
+        child = crossover(red, blue)
+
+        child_cost = gg_total_weight(child)
+
+        self.assertLessEqual(child_cost, red_cost)
+        self.assertLessEqual(child_cost, blue_cost)
+
+        _, response = is_steiner_tree(child, stpg)
+        self.assertTrue(response['all_terminals_in'])
+        self.assertFalse(response['has_cycle'])
+        self.assertTrue(response['all_edges_are_reliable'])
+        self.assertTrue(response['graph_is_connected'])
+        self.assertTrue(response['all_leaves_are_terminals'])
+
+    def test_exchange_edges(self):
+        filename = path.join('datasets', 'test', 'test3.txt')
+        stpg  = ReaderORLibrary().parser(filename)
+
+        red = UGraph()
+        edges = [(1, 3), (3, 8), (8, 5), (5, 6)]
+        for edge in edges:
+            red.add_edge(edge[0], edge[1])
+
+        blue = UGraph
+        edges = [(1, 3), (3, 8), (3, 7), (7, 4), (4, 6)]
+        for edge in edges:
+            blue.add_edge(edge[0], edge[1])
+
+        crossover = PXtree(stpg)
+
+        child = crossover(red, blue)
+
+        _, response = is_steiner_tree(child, stpg)
+        self.assertTrue(response['all_terminals_in'])
+        self.assertFalse(response['has_cycle'])
+        self.assertTrue(response['all_edges_are_reliable'])
+        self.assertTrue(response['graph_is_connected'])
+        self.assertTrue(response['all_leaves_are_terminals'])
