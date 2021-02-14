49b748c38732555a9555ebb450513e79d1e257f0
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2021-02-14 10:52:27 -0300

    It will implement mst random based algorithms

---
+++ ga4stpg/tree/mstcrossover.py
@@ -0,0 +1,24 @@
+
+class CrossoverPrimRST:
+
+    def __init__(self):
+        pass
+
+    def __call__(self, red, blue):
+        pass
+
+
+class CrossoverKruskalRST:
+    def __init__(self):
+        pass
+
+    def __call__(self, red, blue):
+        pass
+
+
+class CrossoverRandomWalkRST:
+    def __init__(self):
+        pass
+
+    def __call__(self, red, blue):
+        pass
