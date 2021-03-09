aad513b083f06d5aa2f06a9ef45aafebc697a306
Giliard Godoi <giliardgodoi@alunos.utfpr.edu.br>
2021-03-09 11:43:17 -0300

    add test for PrimBasedMutation

--- tests/treegraph/test_mutate.py
+++ tests/treegraph/test_mutate.py
@@ -2,11 +2,13 @@ import unittest
 from os import path
 from random import choice
 
-from ga4stpg.graph import UGraph
+from ga4stpg.graph import UGraph, UWGraph
+from ga4stpg.graph.algorithms import prim
 from ga4stpg.graph.reader import ReaderORLibrary
 from ga4stpg.graph.util import is_steiner_tree
+from ga4stpg.tree.evaluation import EvaluateTreeGraph
 from ga4stpg.tree.generate import GenerateBasedRandomWalk
-from ga4stpg.tree.mutate import ReplaceByRandomEdge
+from ga4stpg.tree.mutate import ReplaceByRandomEdge, PrimBasedMutation
 
 class TestMutateReplaceByRandomEdge(unittest.TestCase):
 
@@ -30,3 +32,65 @@ class TestMutateReplaceByRandomEdge(unittest.TestCase):
         self.assertFalse(response['has_cycle'])
         self.assertTrue(response['all_edges_are_reliable'])
         self.assertTrue(response['graph_is_connected'])
+
+
+class TestPrimBasedMutation(unittest.TestCase):
+
+    def test_if_works(self):
+        possibles = [ f'steinb{ii}.txt' for ii in range(10, 19)]
+        filename = choice(possibles)
+        filename = path.join('datasets', 'ORLibrary', filename)
+        stpg  = ReaderORLibrary().parser(filename)
+
+        generator = GenerateBasedRandomWalk(stpg)
+        mutator = PrimBasedMutation(stpg)
+
+        before = generator()
+
+        self.assertIsInstance(before, UGraph)
+        _, response = is_steiner_tree(before, stpg)
+        self.assertTrue(response['all_terminals_in'])
+        self.assertFalse(response['has_cycle'])
+        self.assertTrue(response['all_edges_are_reliable'])
+        self.assertTrue(response['graph_is_connected'])
+
+        after = mutator(before)
+
+        self.assertIsInstance(after, UGraph)
+
+        _, response = is_steiner_tree(after, stpg)
+        self.assertTrue(response['all_terminals_in'])
+        self.assertFalse(response['has_cycle'])
+        self.assertTrue(response['all_edges_are_reliable'])
+        self.assertTrue(response['graph_is_connected'])
+
+    def test_evaluation_cost(self):
+        possibles = [ f'steinb{ii}.txt' for ii in range(10, 19)]
+        filename = choice(possibles)
+        filename = path.join('datasets', 'ORLibrary', filename)
+        stpg  = ReaderORLibrary().parser(filename)
+
+        generator = GenerateBasedRandomWalk(stpg)
+        mutator = PrimBasedMutation(stpg)
+        evaluator = EvaluateTreeGraph(stpg)
+
+        before = generator()
+        after = mutator(before)
+        eval_cost, partitions = evaluator(after)
+
+        self.assertEqual(partitions, 1)
+        self.assertGreater(eval_cost, 0)
+
+        subgraph = UWGraph()
+        vertices = set(before.vertices)
+        graph = stpg.graph
+
+        for v in vertices:
+            for u in graph.adjacent_to(v):
+                if u in vertices:
+                    subgraph.add_edge(v, u, weight=graph.weight(v, u))
+
+
+        _, prim_cost = prim(subgraph, choice(tuple(stpg.terminals)))
+
+        self.assertEqual(prim_cost, eval_cost)
