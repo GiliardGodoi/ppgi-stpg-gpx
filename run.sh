000f3bbad4fcb2ebb1d4b346fb825248fe842c6e
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2021-05-28 15:05:12 -0300

    simulations with Relative Greedy Prim crossover

---
+++ run.sh
@@ -0,0 +1,23 @@
+
+python RelativePrimCrossover/pruner03/RGPrim_Rmst.py > ./saidas/output1.txt
+python RelativePrimCrossover/pruner03/RGPrim_Rpruner-mst.py > ./saidas/output2.txt
+python RelativePrimCrossover/pruner03/RGPrim_Rpruner.py > ./saidas/output3.txt
+python RelativePrimCrossover/pruner03/RGPrim_withoutRepair.py > ./saidas/output4.txt
+
+python RelativePrimCrossover/pruner05/RGPrim_Rpruner-mst.py > ./saidas/output5.txt
+python RelativePrimCrossover/pruner05/RGPrim_Rpruner.py > ./saidas/output6.txt
+
+python RelativePrimCrossover/pruner07/RGPrim_Rpruner-mst.py > ./saidas/output7.txt
+python RelativePrimCrossover/pruner07/RGPrim_Rpruner.py > ./saidas/output8.txt
+
+python RelativePrimCrossover/pruner10/RGPrim_Rpruner-mst.py > ./saidas/output9.txt
+python RelativePrimCrossover/pruner10/RGPrim_Rpruner.py > ./saidas/output10.txt
+
+# cd data
+
+# zip -r RGPrim_Rmst03.zip RGPrim_Rmst03
+# zip -r RGPrim_Rpruner03Mst.zip RGPrim_Rpruner03Mst
+# zip -r RGPrim_Rpruner03.zip RGPrim_Rpruner03
+# zip -r RGPrim_only.zip RGPrim_only
+
+
