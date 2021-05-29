351ed2367445009beac9071ff3f2510b32074b67
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2021-05-28 21:56:03 -0300

    change run.sh file

--- run.sh
+++ run.sh
@@ -1,17 +1,29 @@
 
-python RelativePrimCrossover/pruner03/RGPrim_Rmst.py > ./saidas/output1.txt
-python RelativePrimCrossover/pruner03/RGPrim_Rpruner-mst.py > ./saidas/output2.txt
-python RelativePrimCrossover/pruner03/RGPrim_Rpruner.py > ./saidas/output3.txt
-python RelativePrimCrossover/pruner03/RGPrim_withoutRepair.py > ./saidas/output4.txt
+# python RelativePrimCrossover/pruner03/RGPrim_Rmst.py > ./saidas/output1.txt
+# python RelativePrimCrossover/pruner03/RGPrim_Rpruner-mst.py > ./saidas/output2.txt
+# python RelativePrimCrossover/pruner03/RGPrim_Rpruner.py > ./saidas/output3.txt
+# python RelativePrimCrossover/pruner03/RGPrim_withoutRepair.py > ./saidas/output4.txt
 
-python RelativePrimCrossover/pruner05/RGPrim_Rpruner-mst.py > ./saidas/output5.txt
-python RelativePrimCrossover/pruner05/RGPrim_Rpruner.py > ./saidas/output6.txt
+# python RelativePrimCrossover/pruner05/RGPrim_Rpruner-mst.py > ./saidas/output5.txt
+# python RelativePrimCrossover/pruner05/RGPrim_Rpruner.py > ./saidas/output6.txt
 
-python RelativePrimCrossover/pruner07/RGPrim_Rpruner-mst.py > ./saidas/output7.txt
-python RelativePrimCrossover/pruner07/RGPrim_Rpruner.py > ./saidas/output8.txt
+# python RelativePrimCrossover/pruner07/RGPrim_Rpruner-mst.py > ./saidas/output7.txt
+# python RelativePrimCrossover/pruner07/RGPrim_Rpruner.py > ./saidas/output8.txt
 
-python RelativePrimCrossover/pruner10/RGPrim_Rpruner-mst.py > ./saidas/output9.txt
-python RelativePrimCrossover/pruner10/RGPrim_Rpruner.py > ./saidas/output10.txt
+# python RelativePrimCrossover/pruner10/RGPrim_Rpruner-mst.py > ./saidas/output9.txt
+# python RelativePrimCrossover/pruner10/RGPrim_Rpruner.py > ./saidas/output10.txt
+
+nohup python pruner03/PrimRST_Rpruner-mst.py > prompt/output1.txt
+nohup python pruner03/PXST_Rpruner-mst.py > prompt/output2.txt
+
+nohup python pruner05/PrimRST_Rpruner-mst.py > prompt/output3.txt
+nohup python pruner05/PXST_Rpruner-mst.py > prompt/output4.txt
+
+nohup python pruner07/PrimRST_Rpruner-mst.py > prompt/output5.txt
+nohup python pruner07/PXST_Rpruner-mst.py > prompt/output6.txt
+
+nohup python pruner10/PrimRST_Rpruner-mst.py > prompt/output7.txt
+nohup python pruner10/PXST_Rpruner-mst.py > prompt/output8.txt
 
 # cd data
 
